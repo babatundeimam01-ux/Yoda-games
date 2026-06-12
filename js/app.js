efca0d67f111f357044827f5f5f8cc5636668401
unknown <babatundeimam01@gmail.com>
2026-06-12 10:03:31 -0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,51 @@
+console.log("App is loaded");
+let games = [];
+let container = document.getElementById("gameContainer");
+let search = document.getElementById("search");
+
+let currentCategory = "ALL";
+
+fetch("data/games.json")
+.then(res => res.json())
+.then(data => {
+    games = data;
+    filterGames();
+})
+
+function renderGames(list) {
+    container.innerHTML ="";
+    list.forEach(game =>{
+        container.innerHTML += `<div class="card" onclick="openGame('${game.url}')">
+        <img src="${game.thumb}">
+        <h3> ${game.title}</h3>
+        <p>${game.category}</p>
+        </div>`;
+    } );
+}
+
+function openGame(url){
+    localStorage.setItem("gameUrl", url);
+    window.location.href = "play.html";
+}
+
+search.addEventListener("input", ()=> { 
+    filterGames();
+});
+
+function filterCategory(cat) {
+    currentCategory = cat;
+    filterGames();
+}
+
+function filterGames() {
+    let value = search.value.toLowerCase();
+
+    let filtered = games.filter(game => {
+        let matchSearch = game.title.toLowerCase().includes(value);    
+        let matchCategory = currentCategory === "ALL" || 
+        game.category === currentCategory;
+        
+        return matchSearch && matchCategory;
+   });
+    renderGames(filtered);
+}
